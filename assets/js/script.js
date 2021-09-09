3db03939080cfa20d180af44afc3b362a31d203a
Marco Avila <mavila6@email.arizona.com>
2021-09-08 17:55:43 -0700

    added variables in javascript to select elements and the start quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
+// Query selector variables
 const startBtn = document.querySelector('#startBtn');
+const intro = document.querySelector('.intro');
+const quiz = document.querySelector('.quiz');
+const prompt = document.querySelector('.prompt');
+const results = document.querySelector('.results');
+
+// Event Listeners
+startBtn.addEventListener('click', startQuiz);
+
+// Start the quiz and call timer function
+function startQuiz() {
+    intro.setAttribute('style', 'display: none');
+    quiz.setAttribute('style', 'display: block');
+    timer();
+}
+
+// Start Countdown Timer
+function timer() {
+
+}
