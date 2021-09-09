7b9b717a83ba80b68417f83fe86d38092cfc3869
Marco Avila <mavila6@email.arizona.com>
2021-09-08 18:35:52 -0700

    added timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ const intro = document.querySelector('.intro');
 const quiz = document.querySelector('.quiz');
 const prompt = document.querySelector('.prompt');
 const results = document.querySelector('.results');
+const countdown = document.querySelector('#countdown');
+let timeLeft = 75;
 
 // Event Listeners
 startBtn.addEventListener('click', startQuiz);
@@ -12,10 +14,19 @@ startBtn.addEventListener('click', startQuiz);
 function startQuiz() {
     intro.setAttribute('style', 'display: none');
     quiz.setAttribute('style', 'display: block');
+    countdown.textContent = 'Time:' + timeLeft;
     timer();
-}
+};
 
 // Start Countdown Timer
 function timer() {
-
-}
+    let timeInterval = setInterval(function () {
+        timeLeft --;
+        if (timeLeft >= 1) {
+            countdown.textContent = 'Time: ' + timeLeft;
+        } else {
+            countdown.textContent = 'Time Is Up!';
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+};
