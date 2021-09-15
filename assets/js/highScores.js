445be4bd952e708fc2f6e7b50a419c0f810dc316
Marco Avila <mavila6@email.arizona.com>
2021-09-15 16:24:38 -0700

    added comments

--- assets/js/highScores.js
+++ assets/js/highScores.js
@@ -1,3 +1,4 @@
+// Global Variables
 const scores = document.querySelector('#topScores');
 const back = document.querySelector('.back');
 const clear = document.querySelector('#clear');
@@ -5,22 +6,22 @@ const hs = localStorage.getItem('highScore');
 const intl = localStorage.getItem('initial');
 
 // Event listeners
+// listens for click on back button and returns user to main page
 back.addEventListener('click', e => {
     window.location.replace('./index.html');
 });
-
+//listens for click on clear button and clears the generated p elements conatining the saved highscore and initials
 clear.addEventListener('click', e => {
     scores.innerHTML = "";
     localStorage.clear();
 });
-
+// Dynamically generates a p element containing the initials and saved score and appends it to the html page
 function displayScores() {
     const pEl = document.createElement('p');
     pEl.innerText = "- " + intl + " - " + hs;
     pEl.classList.add('leaderboard');
     scores.appendChild(pEl);
-    console.log('pEl');
 }
-
+// calls the display score function
 displayScores();
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Global variables 
+// Query selector variables
 const startBtn = document.querySelector('#startBtn');
 const intro = document.querySelector('.intro');
 const quiz = document.querySelector('.quiz');
@@ -12,16 +13,19 @@ const countdown = document.querySelector('.countdown');
 const gameEnd = document.querySelector('.gameOver');
 const initials = document.querySelector('#initials');
 const submitBtn = document.querySelector('.submit');
+// Undefined variables to be used later
 let currentQ;
 let score;
+// Countdown time variable
 let timeLeft = 75;
 
-// Event Listener
+// Event Listeners
 startBtn.addEventListener('click', start);
 submitBtn.addEventListener('click', saveScore);
 
 // Start the quiz and call timer function
 function start() {
+    // Display different section of html and call timer function as well as function to display first question in the array
     intro.setAttribute('style', 'display: none');
     quiz.setAttribute('style', 'display: block');
     countdown.textContent = 'Time:' + timeLeft;
@@ -32,6 +36,7 @@ function start() {
 
 // Start Countdown Timer
 function timer() {
+    // Set up timer to decrement every second if time is greater than or equal to 1 and clear the timer and call game over function if time remaining hits 0
     let timeInterval = setInterval(function () {
         if (timeLeft >= 1) {
             timeLeft --;
@@ -46,14 +51,17 @@ function timer() {
 
 // Display initial question
 function displayQuestion(question) {
+    // displays the question and dynamically creates a button for each answer
     questionEl.textContent = question.question
     question.answers.forEach(answer => {
         const button = document.createElement('button');
         button.innerText = answer.text;
         button.classList.add('btn');
+        // checks if answer is correct 
         if (answer.correct) {
             button.dataset.correct = answer.correct
         }
+        // appends answer buttons to page and adds event listener to answer buttons to call check answer function upon a click
         answerBtns.appendChild(button);
         button.addEventListener('click', checkAnswer);
     });
@@ -61,8 +69,10 @@ function displayQuestion(question) {
 
 // Check answer function to display correct/wrong message and decrement time if needed
 function checkAnswer(event) {
+    //two variables representing the answer selected and correct status of an answer
     const selectedAnswer = event.target;
     const rightAnswer = selectedAnswer.dataset.correct;
+    // depending on the answer message for correct/wrong is displayed
     if (rightAnswer === 'true') {
         wrong.setAttribute('style', 'display: none');
         correct.setAttribute('style', 'display: block');
@@ -71,6 +81,7 @@ function checkAnswer(event) {
         wrong.setAttribute('style', 'display: block');
         timeLeft -= 10;
     }
+    //call to next question function
     nextQuestion();
 };
 
@@ -82,12 +93,14 @@ function nextQuestion() {
     if (currentQ < questions.length) {
         displayQuestion(questions[currentQ++]);
     } else {
+        //if there are no more questions call game over function
         gameOver();
     }
 };
 
 // End of quiz function to display the save score page
 function gameOver(){
+    //sets score as remaining time
     score = timeLeft;
     quiz.setAttribute('style', 'display: none');
     prompt.setAttribute('style', 'display: none');
